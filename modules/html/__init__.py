f8a2adf981a90a09dc9c87e7d985a320cde46587
xxtg666 <1747433912@qq.com>
2023-06-10 21:00:33 +0800

    添加html渲染

--- modules/html/__init__.py
+++ modules/html/__init__.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource, reqparse
 from loguru import logger as l
-from flask import Response
 from playwright.sync_api import Playwright, sync_playwright
 import os
 import markdown2
 import random
+import base64
+from PIL import Image
+import io
 
 resources = [
     {"name":"markdown","args":"/markdown"},
@@ -22,13 +24,22 @@ class img(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('html', location='form', type=str)
+        parser.add_argument('github-markdown', location='form', type=str)
         args = parser.parse_args()
-        html = markdown2.markdown(args['html'])
+        args['html'] = args['html'].replace("\\n","")
+        if args['github-markdown'] is None:
+            html = args['html']
+        else:
+            html = open("modules/html/github-markdown-template.html").read().replace("%markdown%",args['html'])
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch(headless=True)
             page = browser.new_page()
             page.set_content(html)
             fpath = os.path.join(os.getcwd(),"cache","".join(random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", k=20))+".png")
-            page.screenshot(path=fpath)
+            page.screenshot(path=fpath,full_page=True)
             browser.close()
-        return Response(open(fpath), mimetype="image/png")
+        img = Image.open(fpath)
+        rawBytes = io.BytesIO()
+        img.save(rawBytes, "PNG")
+        rawBytes.seek(0)
+        return {"message":"success","image_base64":base64.b64encode(rawBytes.read()).decode()}
