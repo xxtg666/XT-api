a3479aa3e1b98c7c71c2c7de59282987b491ec87
This is XiaoDeng <1744793737@qq.com>
2023-06-11 12:35:38 +0800

    使用 autopeo8 格式化代码

--- modules/html/__init__.py
+++ modules/html/__init__.py
@@ -5,16 +5,18 @@ import base64
 
 
 resources = [
-    {"name":"markdown","args":"/markdown"},
-    {"name":"img","args":"/img"},
+    {"name": "markdown", "args": "/markdown"},
+    {"name": "img", "args": "/img"},
 ]
+
 
 class markdown(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('markdown',location='form',type=str)
+        parser.add_argument('markdown', location='form', type=str)
         args = parser.parse_args()
-        return {'message': 'success','html':mistletoe.markdown(args['markdown'])}
+        return {'message': 'success', 'html': mistletoe.markdown(args['markdown'])}
+
 
 class img(Resource):
     def post(self):
@@ -23,19 +25,22 @@ class img(Resource):
         parser.add_argument('github-markdown', location='form', type=str)
         parser.add_argument('timeout', location='form', type=int)
         args = parser.parse_args()
-        args['html'] = args['html'].replace("\\n","")
+        args['html'] = args['html'].replace("\\n", "")
         if args['github-markdown'] is None:
             html = args['html']
         else:
-            html = open("modules/html/github-markdown-template.html").read().replace("%markdown%",args['html'])
+            html = open(
+                "modules/html/github-markdown-template.html").read().replace("%markdown%", args['html'])
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch(headless=True)
             page = browser.new_page()
             page.set_content(html)
             if args['github-markdown'] is None:
-                rawBytes = page.screenshot(type="png", full_page=True,timeout=args['timeout'])
+                rawBytes = page.screenshot(
+                    type="png", full_page=True, timeout=args['timeout'])
             else:
                 element_handle = page.query_selector("//article[@id='md']")
-                rawBytes = element_handle.screenshot(type="png",timeout=args['timeout'])
+                rawBytes = element_handle.screenshot(
+                    type="png", timeout=args['timeout'])
             browser.close()
-        return {"message":"success","image_base64":base64.b64encode(rawBytes).decode()}
+        return {"message": "success", "image_base64": base64.b64encode(rawBytes).decode()}
